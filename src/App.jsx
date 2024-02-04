de70c73756fccd34b6b483920a41975a3cce4dff
José Abel Aguilar Cepeda <jaac16@live.com>
2024-02-04 13:55:35 -0500

    Avance 04/02/2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import './App.css'
-import video from '../../material/fondo2.mp4'
 import Presentation from './components/Presentation'
 
 function App() {
 
   return (
-    <div className='app'>
-        <video autoPlay loop muted className= "video-fondo">
-        <source src={video} type="video/mp4" />
-      </video>
-    </div>
+    <Presentation></Presentation>
   )
 }
 
--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,8 +1,25 @@
+import "./Presentation.css"
+import video from '../../../material/fondo2.mp4'
 
 const Presentation = () => {
  return (
-  <div>
-    <h1>Esta es mi presentacion</h1>
+  <div className="contenedor">
+    <video autoPlay loop muted preload="auto" className= "video-fondo">
+      <source src={video} type="video/mp4" />
+    </video>
+      <div className="contLineas">
+        <div className="linea1"></div>
+        <div className="linea2"></div>
+        <div className="contLinea3-4">
+        <div className="linea3"></div>
+          <div className="linea4"></div>
+        </div>
+      </div>
+      <div className="contTextos">
+        <h1 className='texto1'>BIENVENIDO</h1>
+        <h2 className="texto2">Mi nombre es Jose Abel, soy un desarrollador Full Stack apasionado por crear aplicaciones web innovadoras. Con experiencia en tecnologías como JavaScript, Node.js y React.js, me especializo en transformar ideas en soluciones digitales funcionales y atractivas.</h2>
+        <h2 className="texto2">¡Estoy listo para llevar tu proyecto al siguiente nivel!</h2>
+      </div>
   </div>
   )
 }
