59438c54bf3efa183ebe286c878a63ae0b99dccc
José Abel Aguilar Cepeda <jaac16@live.com>
2024-02-10 22:44:39 -0500

    10/02/2024

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -4,9 +4,11 @@ import video from '../../../material/fondo2.mp4'
 const Presentation = () => {
  return (
   <div className="contenedor">
-    <video autoPlay loop muted preload="auto" className= "video-fondo">
-      <source src={video} type="video/mp4" />
-    </video>
+    <div className="contTextos">
+        <h1 className='texto1'>BIENVENIDO</h1>
+        <h2 className="texto2">Mi nombre es Jose Abel, soy un desarrollador Full Stack apasionado por crear aplicaciones web innovadoras. Con experiencia en tecnologías como JavaScript, Node.js y React.js, me especializo en transformar ideas en soluciones digitales funcionales y atractivas.</h2>
+        <h2 className="texto2">¡Estoy listo para llevar tu proyecto al siguiente nivel!</h2>
+      </div>
       <div className="contLineas">
         <div className="linea1"></div>
         <div className="linea2"></div>
@@ -15,11 +17,10 @@ const Presentation = () => {
           <div className="linea4"></div>
         </div>
       </div>
-      <div className="contTextos">
-        <h1 className='texto1'>BIENVENIDO</h1>
-        <h2 className="texto2">Mi nombre es Jose Abel, soy un desarrollador Full Stack apasionado por crear aplicaciones web innovadoras. Con experiencia en tecnologías como JavaScript, Node.js y React.js, me especializo en transformar ideas en soluciones digitales funcionales y atractivas.</h2>
-        <h2 className="texto2">¡Estoy listo para llevar tu proyecto al siguiente nivel!</h2>
-      </div>
+    <video autoPlay loop muted preload="auto" className= "video-fondo">
+      <source src={video} type="video/mp4" />
+    </video>
+      
   </div>
   )
 }
