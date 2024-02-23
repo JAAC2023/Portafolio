dcd0ffcdfe87e62d2aace650599c36c839609e66
José Abel Aguilar Cepeda <jaac16@live.com>
2024-02-23 18:44:16 -0500

    23/02/2024

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,37 +1,33 @@
 import "./Presentation.css";
 import video from "../../../material/fondo2.mp4";
 import logoLinkedIn from "../assets/linkedin.svg";
+import logoGitGub from "../assets/Github3.png";
 
 const Presentation = () => {
-  const info = [
-    {
-      linkedin: "www.linkedin.com/in/jose-abel-aguilar-cepeda-8a3924284",
-      github: "https://github.com/JAAC2023",
-      name: "José A.",
-    },
-  ];
-
   return (
     <div className="contenedor">
-      {/* <div className="contLinks">
-        <nav>LinkedIn</nav>
-      </div> */}
       <div className="contTextos">
-        {/* <nav>
-          {info.map((el) => (
-            <LinksInfo key={el.name} linkedin={el.linkedin} github={el.github}/>
-          ))}
-        </nav> */}
-        <a href="www.linkedin.com/in/jose-abel-aguilar-cepeda-8a3924284">
-          {logoLinkedIn}
-        </a>
+        <div className="contLinks">
+          <a href="https://www.linkedin.com/in/jose-abel-aguilar-cepeda-8a3924284">
+            <img src={logoLinkedIn} alt="logo linkedin" className="logoLinke" />
+          </a>
+          <a href="https://github.com/JAAC2023">
+            <img src={logoGitGub} alt="logo GitGub" className="logoGit" />
+          </a>
+        </div>
         <h1 className="texto1">BIENVENIDO</h1>
-        <h2 className="texto2">
-          Mi nombre es Jose Abel, soy un desarrollador Full Stack apasionado por
-          crear aplicaciones web innovadoras. Con experiencia en tecnologías
-          como JavaScript, Node.js y React.js, me especializo en transformar
-          ideas en soluciones digitales funcionales y atractivas.
-        </h2>
+        <div className="parrafo">
+          <h2 className="texto2">
+            Mi nombre es Jose Abel, soy un desarrollador Full Stack apasionado
+            por crear aplicaciones web innovadoras. Con experiencia en
+            tecnologías como JavaScript, Node.js y React.js, me especializo en
+            transformar ideas en soluciones digitales funcionales y atractivas.
+          </h2>
+        <div className="proyectos">
+          <h2 className="textoProyectos">PROYECTOS</h2>
+        </div>
+        </div>
+
         <h2 className="texto2">
           ¡Estoy listo para llevar tu proyecto al siguiente nivel!
         </h2>
@@ -52,13 +48,3 @@ const Presentation = () => {
 };
 
 export default Presentation;
-
-function LinksInfo(props) {
-  const { linkedin, github, name } = props;
-  return (
-    <a className="" href={linkedin} target="_blank">
-      <img className="" src={logoLinkedIn} />
-      {name}
-    </a>
-  );
-}
